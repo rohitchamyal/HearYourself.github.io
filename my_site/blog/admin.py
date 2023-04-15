4ab3c2bff68d65eedf330a395f4d9841fda60360
ROHIT CHAMYAL <35057656+rohitchamyal@users.noreply.github.com>
2023-04-15 21:47:46 +0530

    my hearyourself website

---
+++ my_site/blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from.models import Post, Author, Tag
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("Author", "tags", "date",)
+    list_display = ("title", "date", "Author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
+
